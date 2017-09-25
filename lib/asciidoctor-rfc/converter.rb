d75fc1ba71cbbf32b0fa3a31099418c1332e9aec
Nick Nicholas <opoudjis@optusnet.com.au>
2017-09-25 11:10:25 +1000

    seriesInfo

--- lib/asciidoctor-rfc/converter.rb
+++ lib/asciidoctor-rfc/converter.rb
@@ -1,3 +1,5 @@
+require "pp"
+
 module Asciidoctor
   module Rfc
     # A {Converter} implementation that generates RFC XML output, a format used to 
@@ -32,8 +34,9 @@ module Asciidoctor
         nil
       end
 
-      def get_header_attribute node, attr
-        (node.attr? attr) ? %( #{attr}="#{node.attr attr}") : nil
+      def get_header_attribute node, attr, default = nil
+        (node.attr? attr) ? %( #{attr}="#{node.attr attr}") : 
+          default.nil? ? nil : %( #{attr}="#{default}")
       end
 
       def set_header_attribute attr, val
@@ -46,17 +49,25 @@ module Asciidoctor
       end
 
       def document node
+=begin
+=Title
+Author
+:ipr
+:obsoletes
+:updates
+:submissionType
+=end
         result = []
         result << '<?xml version="1.0" encoding="UTF-8"?>'
         ipr = get_header_attribute node, "ipr"
         obsoletes = get_header_attribute node, "obsoletes"
         updates = get_header_attribute node, "updates"
+        submissionType = get_header_attribute node, "submisionType", "IETF"
         t = Time.now.getutc
         preptime = set_header_attribute "preptime", 
           sprintf("%04d-%02d-%02dT%02d:%02d:%02dZ", t.year, t.month, t.day, t.hour, t.min, t.sec)
         version = set_header_attribute "version", "3"
-        # TODO submissionType
-        result << %(<rfc#{document_ns_attributes node}#{ipr}#{obsoletes}#{updates}#{preptime}#{version}>)
+        result << %(<rfc#{document_ns_attributes node}#{ipr}#{obsoletes}#{updates}#{preptime}#{version}#{submissionType}>)
         result << (link node)
         result << (front node)
         result << "<middle>"
@@ -73,8 +84,73 @@ module Asciidoctor
       end
 
       def front node
+=begin
+= Title
+Author
+:stream
+=end
         result = []
-        result << "<title>#{node.doctitle}</title>"
+        abbrev = get_header_attribute node, "abbrev"
+        result << "<front>"
+        abbrev = get_header_attribute node, "abbrev"
+        result << "<title#{abbrev}>#{node.doctitle}</title>"
+        result << (series_info node)
+        result << "</front>"
+      end
+
+      def series_info node
+=begin
+= Title
+Author
+:name rfc-* || Internet-Draft-Name
+:status (of this document)
+:intendedstatus (of internet draft once published as RFC)
+:rfcstatus (of RFC: full-standard|bcp|fyi number or info|exp|historic)
+:stream
+=end
+        result = []
+        status = get_header_attribute node, "status"
+        stream = get_header_attribute node, "stream", "IETF"
+        name = node.attr "docname"
+        rfc = true
+        if not name.nil? and not name.empty?
+          if name =~ /^rfc-?/i
+            name = name.gsub(/^rfc-?/i, "")
+            nameattr = set_header_attribute "name", "RFC"
+          else
+            nameattr = set_header_attribute "name", "Internet-Draft"
+            rfc = false
+          end
+          name = name.gsub(/\.[^\/]+$/, "")
+          value = set_header_attribute "value", name
+          status = get_header_attribute node, "status"
+          result << "<seriesInfo#{nameattr}#{status}#{stream}#{value}/>"
+
+          intendedstatus = node.attr("intendedstatus")
+          if not intendedstatus.nil? and not rfc
+            status = set_header_attribute "status", intendedstatus
+            nameattr = set_header_attribute "name", ""
+            result << "<seriesInfo#{nameattr}#{status}#{value}/>"
+          end
+          rfcstatus = node.attr("rfcstatus")
+          if not rfcstatus.nil? and rfc
+            m = /^(\S+) (\d+)$/.match(rfcstatus)
+            if m.nil?  
+              nameattr = set_header_attribute "name", ""
+              status = set_header_attribute "status", rfcstatus
+              value = set_header_attribute "value", ""
+              result << "<seriesInfo#{nameattr}#{status}#{value}/>"
+            else
+              rfcstatus1 = m[1]
+              rfcstatus2 = m[2]
+              nameattr = set_header_attribute "name", ""
+              status = set_header_attribute "status", rfcstatus1
+              value = set_header_attribute "value", rfcstatus2
+              result << "<seriesInfo#{nameattr}#{status}#{value}/>"
+            end
+          end
+        end
+        result
         # TODO
       end
 
