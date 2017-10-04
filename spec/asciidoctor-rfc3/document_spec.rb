71f6a1cce8e0b2db651390cbd2e9f31086d355cf
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-05 01:06:36 +0200

    finish document spec

--- spec/asciidoctor-rfc3/document_spec.rb
+++ spec/asciidoctor-rfc3/document_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'byebug'
 
 describe Asciidoctor::Rfc3::Converter do
-  it 'renders the minimal document' do
+  it 'renders the minimal document w/ default values' do
     expect(Asciidoctor.convert <<~'INPUT', backend: :rfc3, header_footer: true).to be_equivalent_to <<~'OUTPUT'
       = Document title
       :docName:
@@ -57,6 +57,36 @@ describe Asciidoctor::Rfc3::Converter do
       </author>
       </front><middle>
       </middle>
+      </rfc>
+    OUTPUT
+  end
+
+  it 'renders back matter' do
+    expect(Asciidoctor.convert <<~'INPUT', backend: :rfc3, header_footer: true).to be_equivalent_to <<~'OUTPUT'
+      = Document title
+      :docName:
+      Author
+
+      [appendix]
+      == Appendix
+
+      Lipsum.
+    INPUT
+      <?xml version="1.0" encoding="UTF-8"?>
+      <rfc preptime="1970-01-01T00:00:00Z" version="3" submissionType="IETF">
+      <front>
+      <title>Document title</title>
+      <seriesInfo name="Internet-Draft" stream="IETF" value=""/>
+      <seriesInfo name="" value=""/>
+      <author fullname="Author" initials="A">
+      </author>
+      </front><middle>
+      </middle><back>
+      <section anchor="_appendix" numbered="false">
+      <name>Appendix</name>
+      <t>Lipsum.</t>
+      </section>
+      </back>
       </rfc>
     OUTPUT
   end
