25c6e3ab8f5243382313acfd106ff8b8e46528b2
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-05 21:09:51 +0200

    small spec tweaks

--- spec/asciidoctor-rfc3/paragraph_spec.rb
+++ spec/asciidoctor-rfc3/paragraph_spec.rb
@@ -4,7 +4,7 @@ describe Asciidoctor::Rfc3::Converter do
   it 'renders a paragraph' do
     expect(Asciidoctor.convert <<~'INPUT', backend: :rfc3).to be_equivalent_to <<~'OUTPUT'
       [[id]]
-      [keepWithNext=true, keepWithPrevious=true, foo=bar]
+      [keep-with-next=true, keep-with-previous=true, foo=bar]
       Lorem ipsum.
     INPUT
       <t anchor="id" keepWithNext="true" keepWithPrevious="true">Lorem ipsum.</t>
