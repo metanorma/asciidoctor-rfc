478a18e59e10b3b472d936c0f056280d67d7cf36
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-10-05 21:36:41 +0200

    remove style offenses

--- lib/asciidoctor-rfc.rb
+++ lib/asciidoctor-rfc.rb
@@ -3,4 +3,3 @@ require_relative "asciidoctor-rfc3/version"
 require_relative "asciidoctor-rfc3/converter"
 require_relative "asciidoctor-rfc2/version"
 require_relative "asciidoctor-rfc2/converter"
-
