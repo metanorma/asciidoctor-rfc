4e684f5837b1f488dad045323b50c3c050742c85
Nick Nicholas <opoudjis@optusnet.com.au>
2017-10-09 21:58:11 +1100

    date spec

--- spec/asciidoctor/rfc/v3/date_spec.rb
+++ spec/asciidoctor/rfc/v3/date_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-xdescribe Asciidoctor::RFC::V3::Converter do
+describe Asciidoctor::RFC::V3::Converter do
   it "renders the date" do
     expect(Asciidoctor.convert(<<~'INPUT', backend: :rfc3, header_footer: true)).to be_equivalent_to <<~'OUTPUT'
       = Document title
@@ -9,11 +9,10 @@ xdescribe Asciidoctor::RFC::V3::Converter do
       :revdate: 2070-01-01T00:00:00Z
     INPUT
       <?xml version="1.0" encoding="UTF-8"?>
-      <rfc preptime="1970-01-01T00:00:00Z" version="3" submissionType="IETF">
+      <rfc preptime="1970-01-01T00:00:00Z"
+                version="3" submissionType="IETF">
       <front>
       <title>Document title</title>
-      <seriesInfo name="Internet-Draft" stream="IETF" value=""/>
-      <seriesInfo name="" value=""/>
       <author fullname="Author">
       </author>
 
@@ -33,11 +32,10 @@ xdescribe Asciidoctor::RFC::V3::Converter do
       :date: 1970-01-01T00:00:00Z
     INPUT
       <?xml version="1.0" encoding="UTF-8"?>
-      <rfc preptime="1970-01-01T00:00:00Z" version="3" submissionType="IETF">
+      <rfc preptime="1970-01-01T00:00:00Z"
+               version="3" submissionType="IETF">
       <front>
       <title>Document title</title>
-      <seriesInfo name="Internet-Draft" stream="IETF" value=""/>
-      <seriesInfo name="" value=""/>
       <author fullname="Author">
       </author>
 
@@ -58,11 +56,10 @@ xdescribe Asciidoctor::RFC::V3::Converter do
       :date: 1970-01-01T00:00:00Z
     INPUT
       <?xml version="1.0" encoding="UTF-8"?>
-      <rfc preptime="1970-01-01T00:00:00Z" version="3" submissionType="IETF">
+      <rfc preptime="1970-01-01T00:00:00Z"
+               version="3" submissionType="IETF">
       <front>
       <title>Document title</title>
-      <seriesInfo name="Internet-Draft" stream="IETF" value=""/>
-      <seriesInfo name="" value=""/>
       <author fullname="Author">
       </author>
 
