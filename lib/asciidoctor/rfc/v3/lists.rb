c06410978649e4a6f2199ab210009dd812eb2f13
Nick Nicholas <opoudjis@optusnet.com.au>
2017-10-12 20:15:52 +1100

    raw reference XML in v3

--- lib/asciidoctor/rfc/v3/lists.rb
+++ lib/asciidoctor/rfc/v3/lists.rb
@@ -3,30 +3,39 @@ module Asciidoctor
     module Lists
       # Syntax:
       #   * [[[ref1]]] A
-      #   [quoteTitle=false,target=uri,annotation=x] (optional)
       #   * [[[ref2]]] B
       #   * [[[ref3]]] (Referencegroup: no content)
       #     * [[[ref4]]] C
       #     * [[[ref4]]] D
       # @note ulist repurposed as reference list
       def reflist(node)
-        # TODO reference/front not supported
         result = []
-        node.items.each do |item|
-          # we expect the biblio anchor to be right at the start of the reference
-          if item.blocks?
-            # we expect any list to be embedded, and only one level of embedding
-            # we expect no content in the referencegroup line other than the bibliographic anchor
-            result << "<referencegroup>#{item.text}".gsub(/<referencegroup>\s*\[?<bibanchor="([^"]+)">\]?.*$/, "<referencegroup anchor=\"\\1\">")
-            item.blocks.each { |b| result << reflist(b) }
-            result << "</referencegroup>"
-          else
-            puts item
-            quoteTitle = get_header_attribute item, "quoteTitle"
-            target = get_header_attribute item, "target"
-            annotation = get_header_attribute item, "annotation"
-            # FIXME: [[[x]]] within embedded list is processed as [<bibref>]
-            result << "<reference>#{item.text}</refcontent></reference>".gsub(/<reference>\s*\[?<bibanchor="([^"]+)">\]?\s*/, "<reference#{quoteTitle}#{target}#{annotation} anchor=\"\\1\"><refcontent>")
+        if node.context == :ulist
+          node.items.each do |item|
+            # we expect the biblio anchor to be right at the start of the reference
+            if item.blocks?
+              # we expect any list to be embedded, and only one level of embedding
+              # we expect no content in the referencegroup line other than the bibliographic anchor
+              result << "<referencegroup>#{item.text}".gsub(/<referencegroup>\s*\[?<bibanchor="([^"]+)">\]?.*$/, "<referencegroup anchor=\"\\1\">")
+              item.blocks.each { |b| result << reflist(b) }
+              result << "</referencegroup>"
+            else
+              # quoteTitle = get_header_attribute item, "quoteTitle"
+              # target = get_header_attribute item, "target"
+              # annotation = get_header_attribute item, "annotation"
+              # FIXME: [[[x]]] within embedded list is processed as [<bibref>]
+              result << "<reference>#{item.text}</refcontent></reference>".gsub(/<reference>\s*\[?<bibanchor="([^"]+)">\]?\s*/, "<reference anchor=\"\\1\"><refcontent>")
+            end
+          end
+        elsif node.context == :pass
+          # we expect raw xml
+          node.lines.each do |item|
+            # we expect the biblio anchor to be right at the start of the reference
+            target = get_header_attribute node, "target"
+            # undo XML substitution
+            ref = item.gsub(/\&lt;/, "<").gsub(/\&gt;/, ">")
+            # result << "<reference>#{ref}</reference>".gsub(/<reference>\s*\[?<bibanchor="([^"]+)">\]?\s*/, "<reference#{target} anchor=\"\\1\">")
+            result << ref
           end
         end
         result
@@ -128,7 +137,7 @@ module Asciidoctor
             #id = set_header_attribute "anchor", dd.id # not allowed in asciidoctor
             result << "<dd#{id}>"
             if dd.text?
-                result << dd.text 
+              result << dd.text 
             end
             result << dd.content
             result << "</dd>"
--- spec/asciidoctor/rfc/v3/references_spec.rb
+++ spec/asciidoctor/rfc/v3/references_spec.rb
@@ -100,5 +100,74 @@ describe Asciidoctor::RFC::V3::Converter do
     OUTPUT
   end
 
+  it "renders raw RFC XML as references" do
+    expect(Asciidoctor.convert(<<~'INPUT', backend: :rfc3)).to be_equivalent_to <<~'OUTPUT'
+      = Document title
+      Author
+      :doctype: internet-draft
+
+      == Text
+      Text
+
+      [bibliography]
+      == References
+      ++++
+      <reference anchor='ISO.IEC.10118-3' target='https://www.iso.org/standard/67116.html'>
+        <front>
+          <title>ISO/IEC FDIS 10118-3 -- Information technology -- Security techniques -- Hash-functions -- Part 3: Dedicated hash-functions</title>
+          <author>
+            <organization>International Organization for Standardization</organization>
+            <address>
+              <postal>
+                <street>BIBC II</street>
+                <street>Chemin de Blandonnet 8</street>
+                <street>CP 401</street>
+                <city>Vernier</city>
+                <region>Geneva</region>
+                <code>1214</code>
+                <country>Switzerland</country>
+              </postal>
+              <phone>+41 22 749 01 11</phone>
+              <email>central@iso.org</email>
+              <uri>https://www.iso.org/</uri>
+            </address>
+          </author>
+          <date day='15' month='September' year='2017'/>
+        </front>
+      </reference>
+      ++++
+    INPUT
+       <section anchor="_text" numbered="false">
+       <name>Text</name>
+       <t>Text</t>
+       </section>
+       <references anchor="_references">
+       <name>References</name>
+       <reference anchor='ISO.IEC.10118-3' target='https://www.iso.org/standard/67116.html'>
+         <front>
+           <title>ISO/IEC FDIS 10118-3 -- Information technology -- Security techniques -- Hash-functions -- Part 3: Dedicated hash-functions</title>
+           <author>
+             <organization>International Organization for Standardization</organization>
+             <address>
+               <postal>
+                 <street>BIBC II</street>
+                 <street>Chemin de Blandonnet 8</street>
+                 <street>CP 401</street>
+                 <city>Vernier</city>
+                 <region>Geneva</region>
+                 <code>1214</code>
+                 <country>Switzerland</country>
+               </postal>
+               <phone>+41 22 749 01 11</phone>
+               <email>central@iso.org</email>
+               <uri>https://www.iso.org/</uri>
+             </address>
+           </author>
+           <date day='15' month='September' year='2017'/>
+         </front>
+       </reference>
+       </references>
+    OUTPUT
+  end
 
 end
