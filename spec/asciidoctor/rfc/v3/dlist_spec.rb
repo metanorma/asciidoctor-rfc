38345ea6a513315190e1f2015967ea5c665f6203
Nick Nicholas <opoudjis@optusnet.com.au>
2017-10-20 19:41:47 +1100

    multi para dlist item v3

--- spec/asciidoctor/rfc/v3/dlist_spec.rb
+++ spec/asciidoctor/rfc/v3/dlist_spec.rb
@@ -96,4 +96,26 @@ describe Asciidoctor::RFC::V3::Converter do
       </dl>
     OUTPUT
   end
+  it "permits multi paragraph list items" do
+    expect(Asciidoctor.convert(<<~'INPUT', backend: :rfc3)).to be_equivalent_to <<~'OUTPUT'
+      = Document title
+      Author
+
+      == Section 1
+      Notes::  Note 1.
+      +
+      Note 2.
+      +
+      Note 3.
+    INPUT
+      <section anchor="_section_1" numbered="false">
+         <name>Section 1</name>
+         <dl>
+         <dt>Notes</dt>
+         <dd><t>Note 1.</t><t>Note 2.</t>
+       <t>Note 3.</t></dd>
+       </dl>
+      </section>
+    OUTPUT
+  end
 end
