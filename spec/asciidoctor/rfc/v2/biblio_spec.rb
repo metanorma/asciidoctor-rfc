db550d9d9072f7f3991e410b0492ee7602d0731f
Nick Nicholas <opoudjis@optusnet.com.au>
2017-11-23 21:37:49 +1100

    referencegroup macros

--- spec/asciidoctor/rfc/v2/biblio_spec.rb
+++ spec/asciidoctor/rfc/v2/biblio_spec.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 
 describe Asciidoctor::RFC::V2::Converter do
   it "processes v2 sample biblio file" do
+    system("rm -f spec/examples/refs-v2.new.xml")
     system("bin/asciidoctor-rfc2 -r asciidoctor-bibliography spec/examples/refs-v2.adoc -o spec/examples/refs-v2.new.xml")
     expect(File.read("spec/examples/refs-v2.new.xml")).to be_equivalent_to File.read("spec/examples/refs-v2.xml")
   end
--- spec/asciidoctor/rfc/v3/biblio_spec.rb
+++ spec/asciidoctor/rfc/v3/biblio_spec.rb
@@ -6,6 +6,7 @@ end
 
 describe Asciidoctor::RFC::V3::Converter do
   it "processes v3 sample biblio file" do
+    system("rm -f spec/examples/refs-v3.new.xml")
     system("bin/asciidoctor-rfc3 -r asciidoctor-bibliography spec/examples/refs-v3.adoc -o spec/examples/refs-v3.new.xml")
     expect(remove_prepTime(File.read("spec/examples/refs-v3.new.xml"))).to be_equivalent_to remove_prepTime(File.read("spec/examples/refs-v3.xml"))
   end
