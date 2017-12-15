98d3e33a5d26f7e8572f8994c96224c1d37cb8cb
Nick Nicholas <opoudjis@optusnet.com.au>
2017-12-15 17:53:47 +1100

    rspec cleanup

--- spec/asciidoctor/rfc/v2/text_spec.rb
+++ spec/asciidoctor/rfc/v2/text_spec.rb
@@ -16,7 +16,7 @@ def norm(text)
   text.gsub(%r{<spanx style="strong">(MUST|MUST NOT|REQUIRED|SHALL|SHALL NOT|SHOULD|SHOULD NOT|NOT RECOMMENDED|RECOMMENDED|MAY|OPTIONAL)</spanx>}, "\\1").
     gsub(%r{<t hangText="([^"]+:) ">}, %q{<t hangText="\\1">}).
     gsub(%r{<date year="2009" month="July" day="05"/>}, %q{<date year="2009" month="July" day="5"/>}).
-    gsub(%r{<date year="2017" month="November" day="03"/>}, %q{<date year="2017" month="November" day="03"/>})
+    gsub(%r{<date year="2017" month="November" day="03"/>}, %q{<date year="2017" month="November" day="03"/>}).
     gsub(%r{<author fullname="Editor name"}, %q{<author fullname="Editor Name"})
 end
 
