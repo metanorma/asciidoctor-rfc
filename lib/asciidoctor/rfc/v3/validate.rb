fb950e6d5cf6deb613e0d064317a505d680bfbbc
Ronald Tse <ronald.tse@ribose.com>
2018-01-05 12:54:47 +0800

    Fix puts to write to $stderr

--- lib/asciidoctor/rfc/v3/validate.rb
+++ lib/asciidoctor/rfc/v3/validate.rb
@@ -10,22 +10,22 @@ module Asciidoctor
           # schema = Nokogiri::XML::RelaxNG(File.read(File.join(File.dirname(__FILE__), "validate.rng")).
            #                              gsub(%r{<ref name="svg"/>}, "<externalRef href='#{svg_location}'/>"))
 
-                    filename = File.join(File.dirname(__FILE__), "validate.rng")
+          filename = File.join(File.dirname(__FILE__), "validate.rng")
           schema = Jing.new(filename)
           File.open(".tmp.xml", "w") { |f| f.write(doc.to_xml) }
           begin
             errors = schema.validate(".tmp.xml")
           rescue Jing::Error => e
-            abort "what what what #{e}"
-          end
-          if errors.none?
-            puts "Valid!"
-          else
-            errors.each do |error|
-              puts "#{error[:message]} @ #{error[:line]}:#{error[:column]}"
-            end
+            abort "[asciidoctor-rfc] Validation error: #{e}"
           end
 
+          if errors.none?
+            $stderr.puts "[asciidoctor-rfc] Validation passed."
+          else
+            errors.each do |error|
+              $stderr.puts "[asciidoctor-rfc] #{error[:message]} @ #{error[:line]}:#{error[:column]}"
+            end
+          end
 
         end
       end
